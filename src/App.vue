135f9569e7b5fe948110eb3fa7f2860e2d75a66a
XantX <hp_xantx@hotmail.com>
2022-01-19 13:16:25 -0500

    Se agrego paleta de colores y animaciones para la pantalla de login

--- src/App.vue
+++ src/App.vue
@@ -12,8 +12,33 @@
     margin: 0px;
     padding: 0px;
   }
-@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,200;0,400;0,500;1,900&display=swap');
-#app{
-  font-family: 'Montserrat', sans-serif;
-}
+  :root{
+    --black-0h: #1d2021;
+    --black-0s: #32302f;
+    --black-0: #282828;
+    --black-1: #3c3836;
+    --black-2: #504945;
+    --black-3: #665c54;
+    --black-4: #7c6f64;
+    --white-0: #fbf1c7;
+    --white-1: #ebdbb2;
+    --white-2: #d5c4a1;
+    --white-3: #bdae93;
+    --white-4: #a89984;
+
+    --gray: #928374;
+
+    --blue: #83a598;
+    --aqua: #8ec07c;
+    --yellow: #fabd2f;
+    --orange: #fe8019;
+    --purple: #d3869b;
+    --green: #b8bb26;
+    --red: #fb4934;
+  }
+  @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,200;0,400;0,500;1,900&display=swap');
+  #app{
+    font-family: 'Montserrat', sans-serif;
+    background-color: var(--white-0);
+  }
 </style>
--- src/components/login.vue
+++ src/components/login.vue
@@ -10,21 +10,56 @@
         <input type="password" id="password" placeholder=" " />
         <label class="pass-label">Password</label>
       </div>
+      <div class="button-form">
+        <button class="login-button">Sign in</button>
+        <a class="forget-pass" href="#$">Forget password</a>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'Login',
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.login{
+  color:var(--gray);
+}
+.button-form .forget-pass{
+  text-decoration: none;
+  color: var(--gray);
+  margin-top: 0.5rem;
+}
+.button-form .login-button{
+  border: none;
+  background-color: var(--green);
+  border-radius: 2rem;
+  color: var(--white-0);
+  font-size: 1.2rem;
+  transition: box-shadow 300ms, color 300ms;
+  padding: 1rem 4rem;
+}
+.button-form .login-button:hover{
+  cursor: pointer;
+  color: var(--black-0);
+  -webkit-box-shadow: 33px 29px 21px -17px rgba(0,0,0,0.53);
+  -moz-box-shadow: 33px 29px 21px -17px rgba(0,0,0,0.53);
+  box-shadow: 20px 20px 21px -17px rgba(0,0,0,0.53);
+  background-color: var(--yellow);
+}
+.button-form{
+  margin-top: 1rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
 h1{
   padding: 1.5rem;
-
+  color:var(--aqua);
 }
 .login{
   height:100%;
@@ -48,7 +83,8 @@ label{
   padding-left: 0.3rem;
   transition: top 200ms;
   cursor: text;
-  background-color:white;
+  color:var(--aqua);
+  background-color:var(--white-0);
 }
 #username:focus ~ .user-label,
 #username:not(:placeholder-shown)#username:not(:focus) ~ .user-label
@@ -61,18 +97,19 @@ label{
   top: -0.6rem;
 }
 input{
+  width:17rem;
   border:2px solid gray;
   border-radius: 0.5rem;
   font-family: inherit;
   font-size:inherit;
   outline: none;
   padding: 1rem;
-  background-color:none;
+  background-color:var(--white-0);
 }
 input:hover{
-  border-color: blue;
+  border-color: var(--blue);
 }
 input:focus{
-  border-color: blue; 
+  border-color: var(--blue); 
 }
 </style>
